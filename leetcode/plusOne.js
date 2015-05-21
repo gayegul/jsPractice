20435205cef157d66b4b04f32e5017f2cf8a57b8
Gaye Bulut <gayegul@gmail.com>
2015-05-20 19:57:33 -0700

    add solution

---
+++ leetcode/plusOne.js
@@ -0,0 +1,18 @@
+var plusOne = function(digits) {
+    var carry = 1;
+    var result = [];
+    for(var i = digits.length-1; i >= 0; i--) {
+        var number = digits[i] + carry;
+        if(number < 10) {
+            result.unshift(number);
+            carry = 0;
+        } else {
+            result.unshift(0);
+            carry = 1;
+        }
+    }
+    if(carry === 1) {
+        result.unshift(1);
+    }
+    return result;
+};

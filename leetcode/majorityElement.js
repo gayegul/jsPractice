f01202dd7b678cd1688d94b87f3253e8da6ffac3
Gaye Bulut <gayegul@gmail.com>
2015-05-20 19:58:59 -0700

    add solution

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -1,14 +1,14 @@
 var majorityElement = function(nums) {
     var container = {};
-    var currentMax = 0;
+    var currentHighestCount = 0;
     var result = nums[0];
     for(var i = 0; i < nums.length; i++) {
         if(!container[nums[i]]) {
             container[nums[i]] = 1;
         } else {
             container[nums[i]] = container[nums[i]] + 1;
-            if(currentMax < container[nums[i]]) {
-                currentMax = container[nums[i]];
+            if(currentHighestCount < container[nums[i]]) {
+              currentHighestCount = container[nums[i]];
                 result = nums[i];
             }
         }

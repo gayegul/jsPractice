7f5eefe4b3cd1f7751e55fd48d05a1f27f08e3c9
Gaye Bulut <gayegul@gmail.com>
2015-05-21 20:00:26 -0700

    fix syntax

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -1,17 +1,17 @@
 var majorityElement = function(nums) {
-    var container = {};
-    var currentHighestCount = 0;
-    var result = nums[0];
-    for(var i = 0; i < nums.length; i++) {
-        if(!container[nums[i]]) {
-            container[nums[i]] = 1;
-        } else {
-            container[nums[i]] = container[nums[i]] + 1;
-            if(currentHighestCount < container[nums[i]]) {
-              currentHighestCount = container[nums[i]];
-                result = nums[i];
-            }
-        }
+  var container = {};
+  var currentHighestCount = 0;
+  var result = nums[0];
+  for(var i = 0; i < nums.length; i++) {
+    if(!container[nums[i]]) {
+      container[nums[i]] = 1;
+    } else {
+      container[nums[i]] = container[nums[i]] + 1;
+      if(currentHighestCount < container[nums[i]]) {
+        currentHighestCount = container[nums[i]];
+        result = nums[i];
+      }
     }
-    return result;
+  }
+  return result;
 };

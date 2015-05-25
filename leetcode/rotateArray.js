e96e730609f717febc2f9d17bd950215c220c1d8
Gaye Bulut <gayegul@gmail.com>
2015-05-25 00:13:55 -0700

    fix syntax

--- leetcode/rotateArray.js
+++ leetcode/rotateArray.js
@@ -1,6 +1,6 @@
 var rotate = function(nums, k) {
-    var tempArr = nums.slice();
-    for(var i = 0; i < nums.length; i++) {
-        nums[(i+k)%nums.length] = tempArr[i];
-    }
+  var tempArr = nums.slice();
+  for(var i = 0; i < nums.length; i++) {
+    nums[(i+k)%nums.length] = tempArr[i];
+  }
 };

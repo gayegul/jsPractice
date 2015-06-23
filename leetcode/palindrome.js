58187d1f298dbdb4481fc1b1a1baec35f5843d3e
Gaye Bulut <gayegul@gmail.com>
2015-06-22 22:19:27 -0700

    fix syntax

--- leetcode/palindrome.js
+++ leetcode/palindrome.js
@@ -1,7 +1,7 @@
 var isPalindrome = function(s) {
   s = s.toLowerCase().replace(/\W/g,"");
   for(var i = 0; i < s.length; i++) {
-    if(s[i] !== s[s.length-1-i]) {
+    if(s[i] !== s[s.length - 1 - i]) {
       return false;
     }
   }

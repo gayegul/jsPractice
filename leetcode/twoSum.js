50d645a8b9f4368002ac228bac210e8a1cdd6d1e
Gaye Bulut <gayegul@gmail.com>
2015-06-24 21:29:45 -0700

    fix syntax

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,12 +1,12 @@
 var twoSum = function(nums, target) {
-    var count = {};
-    var result = [];
-    for(var i = 0; i < nums.length; i++) {
-        if(!count[nums[i]]) {
-            count[target - nums[i]] = i+1;
-        } else {
-            result.push(count[nums[i]], i+1);
-        }
+  var count = {};
+  var result = [];
+  for(var i = 0; i < nums.length; i++) {
+    if(!count[nums[i]]) {
+      count[target - nums[i]] = i+1;
+    } else {
+      result.push(count[nums[i]], i+1);
     }
-    return result;
+  }
+  return result;
 };
